41ecdcefe10d5b83a5c02f715d620a41236a9028
Marcio Garcia <marcio.mangar@gmail.com>
2015-06-09 10:35:58 -0300

    exec-once for java and scala instalation

---
+++ puphpet/files/exec-once/install_java.sh
@@ -0,0 +1,24 @@
+echo "--------------------------------------------------------------------------------------------------"
+echo "Installing Oracle Java8"
+echo "--------------------------------------------------------------------------------------------------"
+
+
+
+echo ":: sudo add-apt-repository ppa:webupd8team/java"
+sudo add-apt-repository -y ppa:webupd8team/java
+
+echo ":: sudo apt-get update"
+sudo apt-get update
+
+echo ":: auto accepting Oracle's license"
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+
+echo ":: sudo apt-get install -y oracle-java8-installer"
+sudo apt-get install -y oracle-java8-installer
+
+echo ":: sudo apt-get install -y oracle-java8-set-default"
+sudo apt-get install -y oracle-java8-set-default
+
+
+echo "--------------------------------------------------------------------------------------------------"
